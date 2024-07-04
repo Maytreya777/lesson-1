2662e365e52ad24e871de8f4db69d3597bf6fbf9
Maytreya777 <maytreya777@gmail.com>
2024-07-04 19:36:40 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,7 @@
+example=("Чебурашка")
+print(example[0])# Ч
+print(example[-1])# a
+print(len(example))# 9
+print(example[4:]) # рашка
+print(example[::-1]) #  акшарубеЧ
+print(example[::2]) # Чбрша
